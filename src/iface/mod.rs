51c3ff861a83acfbb7a120bf642a5e2da4647925
Joe <jecolasurdo@gmail.com>
2020-11-03 16:21:46 -0800

    WIP: get_actions_for_state at least building.

--- src/iface/mod.rs
+++ src/iface/mod.rs
@@ -1,30 +1,31 @@
 use mockall::automock;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct LearnerError;
 
 /// Represents the current state of the model.
-pub trait Stater<A>
+#[automock]
+pub trait Stater<A: 'static>
 where
     A: Actioner,
 {
     /// Provides a slice of Actions that are applicable to this state.
-    fn possible_actions(self) -> Vec<A>;
+    fn possible_actions(&mut self) -> Vec<A>;
 
     /// Checks whether or not the supplied action is compatible with this state.
-    fn action_is_compatible(self, actioner: A) -> bool;
+    fn action_is_compatible(&mut self, actioner: A) -> bool;
 
     /// Returns an action of the specified name, or an error if no action exists
     /// of that name fot this state.
-    fn get_action(self, action_name: String) -> Result<A, LearnerError>;
+    fn get_action(&mut self, action_name: String) -> Result<A, LearnerError>;
 
     /// Returns a string representation of this state.
     /// Implementors should take care to ensure this is a consistent hash for a
     /// given state.
-    fn id(self) -> String;
+    fn id(&mut self) -> String;
 
     /// Executes the supplied action.
-    fn apply(self, actioner: A) -> Result<(), LearnerError>;
+    fn apply(&mut self, actioner: A) -> Result<(), LearnerError>;
 }
 
 /// Represents an action that can be applied to the model's current state.
@@ -33,36 +34,36 @@ pub trait Actioner {
     /// Returns a string representation of the given action.
     /// Implementors shoud take care to ensure this is a consistent hash for a
     /// given state.
-    fn id(&self) -> String;
+    fn id(&mut self) -> String;
 }
 
 /// Represents something that is capabile of recommending actions, applying
 /// actions to a given state, and learning based on the transition from one
 /// state to another.
-pub trait Agenter<S, A>
+pub trait Agenter<S, A: 'static>
 where
     S: Stater<A>,
     A: Actioner,
 {
     /// Recommends an action given a state and the model that the agent has
     /// learned thus far.
-    fn recommend_action(self, stater: S) -> Result<A, LearnerError>;
+    fn recommend_action(&mut self, stater: S) -> Result<A, LearnerError>;
 
     /// Applies an action to a given state.
     /// Implementors should take care to ensure that this method returns an
     /// error if the supplied action is not applicable to the specified state.
-    fn transition(self, stater: S, actioner: A) -> Result<(), LearnerError>;
+    fn transition(&mut self, stater: S, actioner: A) -> Result<(), LearnerError>;
 
     // Updates the model for a given state and action using the provided reward.
-    fn learn(self, previous_state: S, action_taken: A, current_state: S, reward: f64);
+    fn learn(&mut self, previous_state: S, action_taken: A, current_state: S, reward: f64);
 }
 
 /// Represents the stats that can be associated with an action.
 pub trait ActionStatter {
-    fn calls(self) -> i64;
-    fn set_calls(self, n: i64);
-    fn q_value_raw(self) -> f64;
-    fn set_q_value_raw(self, q: f64);
+    fn calls(&mut self) -> i64;
+    fn set_calls(&mut self, n: i64);
+    fn q_value_raw(&mut self) -> f64;
+    fn set_q_value_raw(&mut self, q: f64);
     fn q_value_weighted(self) -> f64;
-    fn set_q_value_weighted(self, q: f64);
+    fn set_q_value_weighted(&mut self, q: f64);
 }
--- src/internal/datastructures/mod.rs
+++ src/internal/datastructures/mod.rs
@@ -2,11 +2,11 @@ use std::{collections::HashMap, marker};
 
 use crate::iface::{ActionStatter, Actioner, Stater};
 
-struct QMap<S, A, AS>
+struct QMap<S, A: 'static, AS>
 where
     A: Actioner,
     S: Stater<A>,
-    AS: ActionStatter + Copy,
+    AS: ActionStatter,
 {
     #[allow(dead_code)]
     data: HashMap<String, HashMap<String, AS>>,
@@ -14,11 +14,11 @@ where
     _stater: marker::PhantomData<S>,
 }
 
-impl<S, A, AS> QMap<S, A, AS>
+impl<S, A: 'static, AS> QMap<S, A, AS>
 where
     A: Actioner,
-    S: Stater<A> + Copy,
-    AS: ActionStatter + Copy,
+    S: Stater<A>,
+    AS: ActionStatter,
 {
     #[allow(dead_code)]
     fn new() -> QMap<S, A, AS> {
@@ -29,42 +29,43 @@ where
         }
     }
 
-    fn get_stats(&mut self, state: S, action: A) -> Option<AS> {
-        let actions = self.get_actions_for_state(state);
-        if let Some(action) = actions.get(action.id().as_str()) {
-            return Some(*action);
-        }
-        None
-    }
+    // fn get_stats(&mut self, state: S, action: A) -> Option<AS> {
+    //     let actions = self.get_actions_for_state(state);
+    //     if let Some(action) = actions.get(action.id().as_str()) {
+    //         return Some(action);
+    //     }
+    //     None
+    // }
 
-    fn update_stats(&mut self, state: S, action: A, stats: AS) {
-        self.get_actions_for_state(state).insert(action.id(), stats);
-    }
+    // fn update_stats(&mut self, state: S, action: A, stats: AS) {
+    //     self.get_actions_for_state(state).insert(action.id(), stats);
+    // }
 
-    fn get_actions_for_state(&mut self, state: S) -> HashMap<String, AS> {
+    fn get_actions_for_state(&mut self, state: &mut S) -> &HashMap<String, AS> {
         if !self.data.contains_key(state.id().as_str()) {
             self.data.insert(state.id(), HashMap::new());
         }
-        self.data[state.id().as_str()].clone()
+        &self.data[state.id().as_str()]
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::iface::*;
-    use mockall::predicate::*;
-    use mockall::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use crate::iface::*;
+//     use mockall::predicate::*;
+//     use mockall::*;
 
-    #[test]
-    fn get_stats_no_data() {
-        // let mut mock_stater = MockStater::new();
-        let mut mock_actioner = MockActioner::new();
-        mock_actioner.expect_id().times(1).return_const("test");
-        assert_eq!("test", mock_actioner.id());
+//     #[test]
+//     fn get_stats_no_data() {
+//         let mut state = MockStater::new();
+//         state.expect_id().times(1).return_const("A");
 
-        // let qmap = QMap::new();
-        // let result = qmap.get_stats(state, action);
-        // assert_eq!(None, result);
-    }
-}
+//         let mut action = MockActioner::new();
+//         action.expect_id().times(1).return_const("X");
+
+//         let qmap = QMap::new();
+//         let result = qmap.get_stats(state, action);
+//         assert_eq!(None, result);
+//     }
+// }
