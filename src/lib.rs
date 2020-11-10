b34dd6c348b3d02e4475460716dcd5b1daa396e3
Joe <jecolasurdo@gmail.com>
2020-11-10 13:13:50 -0800

    Excluding mocks crate from non-test builds.

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ pub mod errors;
 pub mod iface;
 pub(crate) mod internal;
 
-
 /// Using manually constructed mocks because (at least at this time), none of
-/// the mocking frameworks seem to cope well with generic traits with
-/// non-static lifetime requirements and functions with generic lifetime
-/// return values.
+/// the mocking frameworks seem to cope well with generic traits that also have
+/// non-static lifetime requirements as well as functions with explicit
+/// lifetimes in return values.
+#[cfg(test)]
 pub(crate) mod mocks;
